79f664e3d0b1e839c9f2424e4cf7620e0c8e1010
yoonpd56 <yoonpd56@gmail.com>
2024-08-20 16:40:01 +0900

    8월 4주차 6문제 / 골드 하위 도장깨기 / ACM Craft 문제 풀이

--- solution.py
+++ solution.py
@@ -1,63 +1,39 @@
-from itertools import combinations
+highest_delay = -1
+MEMOIZATION = {}
+def DFS(delay_list, graph, start_point):
+    global highest_delay
+    global MEMOIZATION
 
-def check_stability(film, D, W, K):
-    for w_iter in range(W):
-        # run이 있는지 확인해보자.
-        prev_char = film[0][w_iter]
-        run_length = 1
-        is_stable = False
-        for d_iter in range(D):
-            if prev_char == film[d_iter][w_iter]:
-                run_length += 1
-                if run_length > K:
-                    # go to next width
-                    is_stable = True
-                    break
-            else:
-                run_length = 2
-            prev_char = film[d_iter][w_iter]
-        if not is_stable:
-            return False
-    return True
-
-
-judgement = False
-def recursively_check(film, D, W, K, inject_comb, depth):
-    global judgement
-    if depth == len(inject_comb):
-        if check_stability(film, D, W, K):
-            judgement = True
-        else:
-            return
-    if judgement:
-        return
-
-    layer = inject_comb[depth]
-
-    for type in range(2):
-        temp_film = [f[:] for f in film]
-        temp_film[layer] = [type for _ in range(W)]
-        recursively_check(temp_film, D, W, K, inject_comb, depth+1)
-
-
-def solve(film, D, W, K):
-    global judgement
-    judgement = False
-
-    for k_iter in range(0, D + 1):
-        for comb in combinations([d for d in range(D)], k_iter):
-            recursively_check(film, D, W, K, comb, 0)
-            if judgement:
-                return k_iter
-
-    return -1
+    if MEMOIZATION.get(start_point, None) is not None:
+        return MEMOIZATION[start_point]
+    
+    delay = delay_list[start_point - 1]
+    ret = []
+    for neighbor in graph[start_point]:
+        ret.append(DFS(delay_list, graph, neighbor))
+    
+    if len(ret) == 0:
+        MEMOIZATION[start_point] = delay
+    else:
+        ret = [item + delay for item in ret]
+        delay = max(ret)
+        MEMOIZATION[start_point] = delay
+    return MEMOIZATION[start_point]
 
 T = int(input())
+
 for t_iter in range(1, T+1):
-    D, W, K = list(map(int, input().split()))
+    highest_delay = -1
+    MEMOIZATION = {}
+    N, K = list(map(int, input().strip().split()))
 
-    input_film = [list(map(int, input().split())) for d_iter in range(D)]
+    build_delay = list(map(int, input().strip().split()))
 
-    result = solve(input_film, D, W, K)
+    dependency_graph = {x+1: set() for x in range(N)}
+    for k_iter in range(K):
+        from_building, to_building = list(map(int, input().split()))
+        dependency_graph[to_building].add(from_building)
+    target_building = int(input())
 
-    print(f"#{t_iter} {result}")
+    DFS(build_delay, dependency_graph, target_building)
+    print(MEMOIZATION[target_building])
