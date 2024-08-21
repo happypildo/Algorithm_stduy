bb08a0dc902b3af51ca380a98a17d896216b0026
yoonpd56 <yoonpd56@gmail.com>
2024-08-21 17:45:55 +0900

    여행경로 문제 너무 풀고싶어요

--- solution.py
+++ solution.py
@@ -1,42 +1,55 @@
-N = 200
-
-patterns = {x: set() for x in range(N+10)}
-def generate_pattern(repeat_str, concat_str, N):
-    global patterns
-
-    temp_str = concat_str[0] + concat_str[1]
-    if temp_str in patterns[len(temp_str)]: return
-    else: patterns[len(temp_str)].add(temp_str)
-    if len(temp_str) > N: return
-
-    generate_pattern(repeat_str, [concat_str[0] + repeat_str[0], concat_str[1] + repeat_str[1]], N)
-    generate_pattern(repeat_str, [concat_str[0], concat_str[1] + repeat_str[1]], N)
-    generate_pattern(repeat_str, [concat_str[0] + repeat_str[0], concat_str[1]], N)
-
-generate_pattern(["0", "1"], ["100", "1"], N)
-patterns[0].add("")
-patterns[2].add("01")
-
-def is_valid(remain):
-    if remain in patterns[len(remain)]:
-        return True
+fastest_path = ""
+def DFS(source, path_dict, ticket_set, used_ticket, travel):
+    global fastest_path
+    if path_dict.get(source, None) is None:
+        if len(travel) == len(ticket_set) * 3 * 2:
+            if fastest_path > travel:
+                fastest_path = travel
+        return
+    if len(travel) == len(ticket_set) * 3 * 2:
+        if fastest_path > travel:
+            fastest_path = travel
+        return
+    # if travel > fastest_path:
+    #     return
     
-    temp_remain = remain[:]
-    if remain[0:2] == "01":
-        temp_remain = temp_remain[2:]
-        
-    ret = False
-    for seg_len in range(len(temp_remain), 0, -1):
-        if temp_remain[:seg_len] in patterns[seg_len]:
-            # print(patterns[seg_len], seg_len, temp_remain[:seg_len], temp_remain[seg_len:])
-            ret = ret | is_valid(temp_remain[seg_len:])
-    return ret
+    for destination in path_dict[source]:
+        if (source, destination) not in used_ticket:
+            temp_used_ticket = used_ticket | set([(source, destination)])
 
-T = int(input())
-for t_iter in range(1, T+1):
-    input_line = input().rstrip()
+            DFS(destination, path_dict, ticket_set, temp_used_ticket, travel + f"{source}{destination}")
 
-    if is_valid(input_line):
-        print("YES")
-    else:
-        print("NO")
+
+def solution(tickets):
+    global fastest_path
+
+    path_dict = {}
+    ticket_set = set()
+    for source, destination in tickets:
+        ticket_set.add((source, destination))
+        if path_dict.get(source, None) is None:
+            path_dict[source] = [destination]
+        else:
+            path_dict[source].append(destination)
+
+    # for key in path_dict.keys():
+    #     path_dict[key] = sorted(path_dict[key])
+
+    fastest_path = "a" * len(ticket_set) * 3 * 2
+    DFS(source="ICN", path_dict=path_dict, ticket_set=ticket_set, used_ticket=set(), travel="")
+    
+    # PROCESSING
+    answer = []
+    for offset in range(0, len(fastest_path), 6):
+        answer.append(fastest_path[offset:offset+3])
+    answer.append(fastest_path[-3:])
+
+    return answer
+
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"], ["SFO", "ICN"]]))
+# print(
+#     solution(
+#         [["ICN", "A"], ["ICN", "B"], ["ICN", "C"], ["A", "AA"], ["A", "BB"]]
+#     )
+# )
