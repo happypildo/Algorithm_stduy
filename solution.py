5df2fd4c6993de08393c91b889828886cb3e7e4c
yoonpd56 <yoonpd56@gmail.com>
2024-08-28 17:49:38 +0900

    LIS 공부 필요...

--- solution.py
+++ solution.py
@@ -1,6 +1,27 @@
 N = int(input())
 
-DP = [1, 2]
-for i in range(2, N):
-    DP.append(DP[i - 1] % 10007 + DP[i - 2] % 10007)
-print(DP[N-1] % 10007)
+arr = list(map(int, input().split()))
+DP = [1 for _ in range(N)]
+
+latest_largest_idx = 1
+for i in range(N):
+    if i == 0: continue
+
+    found = False
+    candi_DP = -1
+    candi_lli = -1
+    for j in range(latest_largest_idx, -1, -1):
+        if arr[i] > arr[j]:
+            if DP[j] + 1 >= DP[i - 1]:
+                if candi_DP < DP[j] + 1:
+                    candi_DP = DP[j] + 1
+                    candi_lli = i
+                found = True
+    if found:
+        DP[i] = candi_DP
+        latest_largest_idx = i
+    else:
+        DP[i] = DP[i - 1]
+        # latest_largest_idx = i
+print(DP)
+print(DP[-1])
