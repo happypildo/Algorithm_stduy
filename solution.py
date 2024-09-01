f0e2a1ab75fd31620704c10cb71f96b802a40155
happypildo <yoonpd.workspace@gmail.com>
2024-09-01 23:21:16 +0900

    9월 1주차 5문제 / 과목평가준비 / 플로이드  문제 풀이

--- solution.py
+++ solution.py
@@ -1,30 +1,34 @@
 """
 # 문제 링크: https://www.acmicpc.net/problem/11403
 # 문제 설명:
-- 무방향 그래프가 주어졌을 때, i->j로 가는 길이가 양수인 경로가 있는지 없는지 구하시오.
+- 무방향 그래프가 주어졌을 때, i->j로 가는 최소 길이를 모두 구하시오.
 # 접근법:
 - 모든 정점 쌍에 대한 거리를 구해야 하기 때문에 Floyd 알고리즘을 사용함
 """
 
+def floyd():
+    pass
+
 N = int(input())
-graph = [list(map(int, input().split())) for _ in range(N)]
+M = int(input())
 
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 0: graph[i][j] = float('inf')
+graph = [[float('inf') for _ in range(N + 1)] for _ in range(N + 1)]
+for m_iter in range(M):
+    s, e, w = list(map(int, input().split()))
+    graph[s][e] = graph[s][e] if graph[s][e] < w else w
 
-for stopover in range(N):
-    for i in range(N):
-        for j in range(N):
-            if i == stopover or j == stopover: continue
+for stopover in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            if i == stopover or j == stopover or i == j: continue
             graph[i][j] = min(
                 graph[i][j],
                 graph[i][stopover] + graph[stopover][j]
-            )
+            )            
 
 
-for i in range(N):
-    for j in range(N):
-        answer = 1 if graph[i][j] != float('inf') else 0
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        answer = graph[i][j] if graph[i][j] != float('inf') else 0
         print(answer, end=" ")
     print()
