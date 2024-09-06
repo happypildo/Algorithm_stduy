bc79a257b0386d8aa601807280391240ac3b04b3
happypildo <yoonpd.workspace@gmail.com>
2024-09-06 10:05:19 +0900

    과제 문제 풀이

--- solution.py
+++ solution.py
@@ -1,83 +1,18 @@
-import sys
-sys.setrecursionlimit(10 ** 6)
+T = int(input())
 
-original_path = [set(), set()]
-
-
-def DFS(graph, graph_keys, node, start_node, target_node, is_visited, is_work):
-    global original_path
+for t_iter in range(1, T+1):
+    N = int(input())
     
-    if target_node in original_path[is_work]:
-        # 길은 하나 만 알고 있어도 된다.
-        return
+    happy_days = [int(input()) - 1 for _ in range(N)]
+    multipliers = [happy_days[1]]
     
-    if node not in graph_keys: return
-    if node in original_path[is_work]: return
-    if node == target_node:
-        original_path[is_work].update(is_visited)
-        original_path[is_work].add(node)
-        return
-        
-    for neighbor in graph[node]:
-        if neighbor not in is_visited:
-            DFS(graph, graph_keys, neighbor, start_node, target_node, is_visited | set([neighbor]), is_work)
-
-    return 
-
-def reDFS(graph, graph_keys, cur_node, start_node, end_node, is_visited, is_work):
-    global original_path
+    for happy in happy_days[2:]:
+        is_there = False
+        for mul in multipliers:
+            if happy % mul == 0:
+                is_there = True
+                break
+        if not is_there: multipliers.append(happy)
     
-    # DFS를 돌면서 다시 original path로 회귀할 수 있는지 체크
-    if cur_node in original_path[is_work]:
-        return True
+    print(f"#{t_iter} {len(multipliers)}")
     
-    ret = False
-    if cur_node not in graph_keys: return False
-    for neighbor in graph[cur_node]:
-        if neighbor in is_visited: continue
-        result = reDFS(graph, graph_keys, neighbor, start_node, end_node, is_visited | set([neighbor]), is_work)
-        if result:
-            original_path[is_work].add(neighbor)
-        ret |= result
-    if ret:
-        original_path[is_work].add(cur_node)
-    return ret
-                
-        
-
-from collections import deque
-def reBFS(graph, graph_keys, start_node, end_node):
-    ret = []
-    for i in [0, 1]:
-        s = start_node if i == 0 else end_node
-        e = end_node if i == 0 else start_node
-        
-        temp_original_path = original_path[i].copy()
-        for path in temp_original_path:
-            if path == e: continue
-            for neighbor in graph[path]:
-                if neighbor not in original_path:
-                    reDFS(graph, graph_keys, neighbor, s, e, set(), i)
-
-    return original_path
-    
-
-N, M = map(int,sys.stdin.readline().split())
-
-graph = {}
-graph_keys = set()
-for _ in range(M):
-    s, e = map(int,sys.stdin.readline().split())
-    if graph.get(s, None) is None:
-        graph[s] = set([e])
-        graph_keys.add(s)
-    else: 
-        graph[s].add(e)
-
-S, T = map(int,sys.stdin.readline().split())
-
-DFS(graph, graph_keys, S, T, T, set([S]), 0)
-DFS(graph, graph_keys, T, S, S, set([T]), 1)
-
-ret = reBFS(graph, graph_keys, S, T)
-print(len(ret[0] & ret[1]) - 2)
