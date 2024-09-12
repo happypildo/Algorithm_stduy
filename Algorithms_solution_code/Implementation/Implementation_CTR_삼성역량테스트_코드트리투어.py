4b43990d96aced5f316f8fecce9c915e8e17023a
happypildo <yoonpd.workspace@gmail.com>
2024-09-12 16:29:04 +0900

    9월 2주차 2문제 / 삼성역량테스트 / 코드트리 문제 풀이

---
+++ Algorithms_solution_code/Implementation/Implementation_CTR_삼성역량테스트_코드트리투어.py
@@ -0,0 +1,149 @@
+import heapq
+
+MAX_GRAPH = 2001
+MAX_NODES = 2001
+MAX_PRODUCTIONS = 30001
+IS_PRODUCTION = [False] * MAX_PRODUCTIONS
+
+
+class Production:
+    def __init__(self, p_id, rev, dest, profit):
+        self.p_id = p_id
+        self.revenue = rev
+        self.destination = dest
+        self.profit = profit
+
+    def __lt__(self, other):
+        if self.profit > other.profit:
+            return True
+        elif self.profit == other.profit:
+            if self.p_id < other.p_id:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+
+def dijkstra(graph, start):
+    distances = [float('inf') for _ in range(MAX_GRAPH)]
+    distances[start] = 0
+
+    min_heap = []
+    heapq.heappush(min_heap, [0, start])
+
+    while min_heap:
+        dist, node = heapq.heappop(min_heap)
+
+        if graph.get(node, None) is None:
+            continue
+        for neighbor, weight in graph[node]:
+            original_dist = distances[neighbor]
+            incoming_dist = distances[node] + weight
+
+            if incoming_dist < original_dist:
+                distances[neighbor] = incoming_dist
+                heapq.heappush(min_heap, [incoming_dist, neighbor])
+
+    return distances
+
+
+def make_production(distances, productions, p_id, rev, dest):
+    global IS_PRODUCTION
+
+    profit = rev - distances[dest]
+
+    new_prod = Production(p_id, rev, dest, profit)
+    IS_PRODUCTION[p_id] = True
+    heapq.heappush(productions, new_prod)
+
+    return productions
+
+
+def delete_production(p_id):
+    global IS_PRODUCTION
+
+    if IS_PRODUCTION[p_id]:
+        IS_PRODUCTION[p_id] = False
+
+
+def recommend_production(productions):
+    global IS_PRODUCTION
+
+    if len(productions) == 0:
+        return -1, productions
+    else:
+        while productions:
+            target_prod = heapq.heappop(productions)
+
+            if IS_PRODUCTION[target_prod.p_id]:
+                if target_prod.profit >= 0:
+                    IS_PRODUCTION[target_prod.p_id] = False
+                    return target_prod.p_id, productions
+                else:
+                    heapq.heappush(productions, target_prod)
+                    return -1, productions
+
+        return -1, productions
+
+
+def change_start_point(distances, productions):
+    global IS_PRODUCTION
+
+    new_heap = []
+    for prod in productions:
+        if not IS_PRODUCTION[prod.p_id]:
+            continue
+
+        new_heap = make_production(distances, new_heap,
+                                   prod.p_id, prod.revenue, prod.destination)
+
+    return distances, new_heap
+
+
+distance_map = [float('inf') for _ in range(MAX_NODES)]
+prods_heap = []
+heapq.heapify(prods_heap)
+
+n, m = 0, 0
+
+graph = {}
+nodes = set()
+
+start = 0
+
+prev_order = -1
+Q = int(input())
+for q in range(Q):
+    orders = list(map(int, input().split()))
+
+    order_num = orders[0]
+    if order_num == 100:
+        # 그래프 생성
+        n, m, *pairs = orders[1:]
+        for i in range(len(pairs) // 3):
+            v, u, w = pairs[i * 3:(i + 1) * 3]
+            if graph.get(v, None) is None:
+                graph[v] = set()
+            if graph.get(u, None) is None:
+                graph[u] = set()
+            graph[v].add((u, w))
+            graph[u].add((v, w))
+
+        distance_map = dijkstra(graph, 0)
+    elif order_num == 200:
+        # 상품 생성
+        prods_heap = make_production(distance_map, prods_heap, orders[1], orders[2], orders[3])
+    elif order_num == 300:
+        # 상품 삭제
+        delete_production(orders[1])
+    elif order_num == 400:
+        # 최적 상품 제안
+        answer, prods_heap = recommend_production(prods_heap)
+        print(answer)
+    elif order_num == 500:
+        # 출발지 변경
+        start = orders[1]
+        distance_map = dijkstra(graph, start)
+
+        distance_map, prods_heap = change_start_point(distance_map, prods_heap)
