587f205f25280457264d2c9402737727ff151dee
SSAFY <yoonpd56@gmail.com>
2024-10-04 17:11:27 +0900

    아직 못풀었음 ㅠ..

--- solution.py
+++ solution.py
@@ -1,38 +1,47 @@
-from collections import deque
+DIRECTION = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]
+PUT_LOCATIONS = set()
+RESULT = 0
 
 
-class Node:
-    def __init__(self, node_id, parent_id, auth):
-        self.node_id = node_id
-        self.parent_id = parent_id
-        self.authority = auth
+def update_impossible_loc(N, impossible_location_set, cur_point):
+    temp_location_set = impossible_location_set | {cur_point}
+    for dx, dy in DIRECTION:
+        temp_x, temp_y = cur_point[0] + dx, cur_point[1] + dy
 
-        self.is_on = True
-        self.child_set = set()
-        self.alarm_set = set()
+        while (-1 < temp_x < N) and (-1 < temp_y < N):
+            temp_location_set.add((temp_x, temp_y))
+            temp_x, temp_y = temp_x + dx, temp_y + dy
+
+    return temp_location_set
 
 
-class ChatRoom:
-    def __init__(self, num_of_nodes, parent_ids, auths):
-        self.chats = {}
-        to_be_added = []
-        for i in range(1, num_of_nodes + 1):
-            self.chats[i] = Node(i, parent_ids[i - 1], auths[i - 1])
-            if self.chats.get(parent_ids[i - 1], None) is not None:
-                self.chats[parent_ids[i - 1]].child_set.add(i)
-            else:
-                to_be_added.append((parent_ids[i - 1], i))
+SELECTED = set()
+def DFS(N, all_location_set, impossible_location_set, put_location, depth, selected_set):
+    global RESULT
+    global PUT_LOCATIONS
+    global SELECTED
 
-        for p_id, c_id in to_be_added:
-            self.chats[p_id].child_set.add(c_id)
-
-    def find_alarm_set(self, target_node_id):
-        # 타겟 노드에서 아래로 내려가면서 알람 셋을 설정한다.
-
+    if depth == N:
+        RESULT += 1
+        SELECTED.add(tuple(selected_set))
+        return
+    if len(all_location_set - impossible_location_set) == 0:
         return
 
-    def toggle(self, node_id):
-        self.chats[node_id].is_on = False if self.chats[node_id].is_on else True
+    temp_location_set = update_impossible_loc(N, impossible_location_set, put_location)
+    for loc in all_location_set - temp_location_set:
+        temp_selected_set = selected_set | {put_location}
+        DFS(N, all_location_set, temp_location_set, loc, depth + 1, temp_selected_set)
 
-    def change_authority(self, node_id, power):
-        self.chats[node_id].authority = power
+
+chess_size = int(input())
+all_loc = set([
+    (i, j) for j in range(chess_size) for i in range(chess_size)
+])
+
+for i in range(chess_size // 2):
+    for j in range(i + 1):
+        print((i, j))
+        DFS(chess_size, all_loc, set(), (i, j), 1, {(i, j)})
+print(RESULT)
+print(len(SELECTED))
