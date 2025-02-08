3f3c6e9f2b1622f6ac59b4ad9954ccf60a726f49
unknown <20171621@o365.hanbat.ac.kr>
2025-02-08 21:52:40 +0900

    BOJ S4 1920 Data Structure

--- Study_cpp/Algorithm_solution_codes/BOJ_S4_1920_수찾기/BOJ_S4_1920_수찾기/code.cpp
+++ Study_cpp/Algorithm_solution_codes/BOJ_S4_1920_수찾기/BOJ_S4_1920_수찾기/code.cpp
@@ -1,87 +1,97 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void swap(int a, int b, long* arr) {
-	int temp = arr[a];
-	arr[a] = arr[b];
-	arr[b] = temp;
-}
+class HashTable {
+private:
+	int size = 100003;
+	int modular_value = 100003;
+	int** table;
+	bool** is_linked;
+	int chain_size = 5;
 
-void quick_sort(int start, int end, long* arr) {
-	if (start >= end) return;
-	int pivot = start;
-	int front = start + 1;
-	int rear = end;
-
-	while (front <= rear) {
-		bool f = arr[pivot] >= arr[front];
-		bool r = arr[pivot] <= arr[rear];
-		if (f && r) {
-			front++;
-			rear--;
-		} 
-		else if (!f && !r) {
-			swap(front, rear, arr);
-			front++;
-			rear--;
-		}
-		else if (f) {
-			front++;
-		}
-		else if (r) {
-			rear--;
+public:
+	HashTable() {
+		table = new int* [size];
+		is_linked = new bool* [size];
+		for (int i = 0; i < size; i++) {
+			table[i] = new int[chain_size];
+			is_linked[i] = new bool[chain_size];
+			for (int j = 0; j < chain_size; j++) {
+				is_linked[i][j] = false;
+			}
 		}
 	}
-	swap(pivot, rear, arr);
-
-	//cout << "[Quick Sort] " << start << " " << end << endl;
-	//for (int i = 0; i < 10; i++) {
-	//	cout << arr[i] << "\t";
-	//}
-	//cout << endl;
-
-	quick_sort(start, rear - 1, arr);
-	quick_sort(rear + 1, end, arr);
-}
-
-bool binary_search(int start, int end, int N, long target, long* arr) {
-	if (start == end) {
-		return target == arr[start];
+	~HashTable() {
+		for (int i = 0; i < size; i++) {
+			delete[] table[i];
+			delete[] is_linked[i];
+		}
+		//delete[] table;
+		//delete[] is_linked;
 	}
-	if (start > end) {
+	void insert(int x) {
+		int hash_value = (x % modular_value + modular_value) % modular_value;
+
+		//cout << "[HashTable - insert]" << endl;
+		//cout << "called" << endl;
+		bool inserted = true;
+		while (inserted) {
+			for (int i = 0; i < chain_size; i++) {
+				if (is_linked[hash_value][i] && x == table[hash_value][i]) return;
+				else if (is_linked[hash_value][i]) continue;
+				else {
+					inserted = false;
+					is_linked[hash_value][i] = true;
+					table[hash_value][i] = x;
+					break;
+				}
+			}
+			hash_value++;
+			if (hash_value > size - 1) hash_value = 0;
+		}
+		//cout << "Over" << endl;
+	}
+	bool is_in(int x) {
+		int hash_value = (x % modular_value + modular_value) % modular_value;
+		int original_hash_value = hash_value;
+		while (true) {
+			for (int i = 0; i < chain_size; i++) {
+				if (is_linked[hash_value][i] && table[hash_value][i] == x) return true;
+				else if (!is_linked[hash_value][i]) return false;
+			}
+			hash_value++;
+			if (hash_value > size - 1) hash_value = 0;
+
+			if (hash_value == original_hash_value) return false;
+		}
+
 		return false;
 	}
-
-	int mid = (int)((start + end) / 2);
-	if (target < arr[mid]) {
-		return binary_search(start, mid - 1, N, target, arr);
-	}
-	else if (target == arr[mid]) return true;
-	else {
-		return binary_search(mid + 1, end, N, target, arr);
-	}
-}
+};
 
 int main() {
-	int N;
+	HashTable hash_table;
+
+	int N = 0;
 	cin >> N;
 
-	long* arr = new long[N];
 	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
+		int x = 0;
+		cin >> x;
+		hash_table.insert(x);
 	}
 
-	quick_sort(0, N - 1, arr);
-
-	int M;
+	int M = 0;
 	cin >> M;
-
+	string ret = "";
 	for (int i = 0; i < M; i++) {
-		long target = 0;
-		cin >> target;
-		cout << binary_search(0, N, N, target, arr) << endl;
+		int x = 0;
+		cin >> x;
+		if (hash_table.is_in(x)) ret += "1\n";
+		else ret += "0\n";
 	}
 
-	delete[] arr;
+	cout << ret << endl;
 }
